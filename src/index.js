94d21e98ca9e4245abc609a180fe30bf3d9118d8
Caroline Levison <caroline.levison@cloudinary.com>
2023-12-14 12:13:26 +0000

    Modern JS changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
